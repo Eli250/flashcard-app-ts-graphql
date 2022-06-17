687b4ea3eaa8593435d85efd59606d575b1aa96f
Eli250 <hirwaeli@outlook.com>
2022-06-17 17:31:03 +0200

    trigger deployment

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,17 +6,10 @@ export interface AuthTokenPayload {
   userId: number;
 }
 
-export function decodeAuthHeader(
-  authHeader: String
-): AuthTokenPayload | string {
-  try {
-    const token = authHeader.replace("Bearer ", "");
-    if (!token) {
-      throw new Error("No token found");
-    }
-    return jwt.verify(token, APP_SECRET) as AuthTokenPayload;
-  } catch (error) {
-    console.log(error);
-    return "Something went  wrong";
+export function decodeAuthHeader(authHeader: String): AuthTokenPayload {
+  const token = authHeader.replace("Bearer ", "");
+  if (!token) {
+    throw new Error("No token found");
   }
+  return jwt.verify(token, APP_SECRET) as AuthTokenPayload;
 }
